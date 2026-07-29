a0710416682829f740203b923b09799eea560858
aswinvijayan-vonnue <aswin.vijayan@vonnue.com>
2026-07-29 09:44:55 +0530

    feat:task 7 of day 1

---
+++ src/types/test-library.d.ts
@@ -0,0 +1,7 @@
+declare module '#test-library' {
+  export function findLength(arr: number[]): number;
+  export function addNumbers(num1: number, num2: number): number;
+  export const maxVal: number;
+}
+
+declare const _APP_VERSION_: string;
